fb2c8167a66cb32873a528fc209e5058320eb10f
David Aleu Mosegui <david.aleu.16@gmail.com>
2019-10-06 05:32:32 +0200

    done friends

--- client/src/components/sections/friends/Friends.tsx
+++ client/src/components/sections/friends/Friends.tsx
@@ -5,6 +5,7 @@ import { IGitInspectState } from '../../../state';
 import { NextPage } from '../../../actions/nextPage';
 import { Button } from '@material-ui/core';
 import '../../../style/section.css';
+import '../../../style/friends.css';
 
 class Friends extends React.Component<any, {}> {
 
@@ -13,14 +14,40 @@ class Friends extends React.Component<any, {}> {
     }
 
     public render(): React.ReactElement<any>{
+
+        console.log("CONT",this.props.user_main_data.contributors);
+
         return(
             <Card className="sectionCard">
-                <div className="headerSection">
-                    <h1 className="profileName">Here are your contributors...</h1>
-                </div>
-                <div className="sectionContent"> 
-
-                </div>
+                {
+                    this.props.user_main_data.contributors.length==0 ?
+                        (<div className="headerSection">
+                            <h1 className="profileName">You don't have any contributor... It's so sad</h1>
+                        </div>) :
+                        (<div>
+                            <div className="headerSection">
+                                <h1 className="profileName">Here are your contributors...</h1>
+                            </div>
+                            <div className="sectionContent"> 
+                                {this.props.user_main_data.contributors.map((contributor: any)=>{
+                                    return(
+                                        <div className="friendsListCardContent">
+                                            <div className="cardProfilePhoto">
+                                                <img src={contributor.photo}/>
+                                            </div>
+                                            <div className="cardTextContainer">
+                                                <div className="friendFullName">{contributor.label}</div>
+                                                <div className="friendExperience">{contributor.commits+" commits"}</div>
+                                            </div>
+                                            {/* <div className="cardRightArrow">
+                                                <img className='friendsIcon' src={rightArrow}></img>
+                                            </div> */}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>)
+                }
                 <div className="footerContent">
                     <Button 
                         variant="contained" 
